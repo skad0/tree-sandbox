4ed208e0eab5b344858ea0f52ddc9b6d9e47896a
Anton Krichevskii <mrskadi0@gmail.com>
2017-09-05 22:29:19 +0300

    Rewrite flattenTree tests

--- src/helpers/flattenTree.spec.js
+++ src/helpers/flattenTree.spec.js
@@ -1,86 +1,97 @@
 import flatten from './flattenTree';
-import _ from 'lodash';
-
-const commonTree = {
-    id: 1,
-    title: 'Root node',
-    payload: {},
-    folder: true,
-    children: [
-        {
-            id: 3,
-            title: 'Second Child',
-            payload: { chmod: '777' },
-            owner: 'admin',
-            folder: false
-        }
-    ]
-};
-const customChildren = Object.assign(
-    _.omit(commonTree, ['children']),
-    { descedants: [].concat(commonTree.children) }
-);
-const commonResult = [
-    {
-        id: 1,
-        title: 'Root node',
-        folder: true,
-        parentId: undefined,
-        expandable: true,
-        payload: {}
-    },
-    {
-        id: 3,
-        title: 'Second Child',
-        folder: false,
-        parentId: 1,
-        expandable: false,
-        payload: { owner: 'admin', chmod: '777' }
-    }
-];
-const customPayload = {
-    id: 1,
-    title: 'folder',
-    folder: false,
-    additional: { owner: 'admin' },
-    icon: 'arrow'
-};
-const customPayloadResult = [
-    {
-        id: 1,
-        title: 'folder',
-        folder: false,
-        parentId: undefined,
-        expandable: false,
-        additional: { icon: 'arrow', owner: 'admin' }
-    }
-];
-
-const tests = [
-    {
-        input: commonTree,
-        expected: commonResult,
-        message: 'should correctly build list with correct payloads',
-        options: {}
-    },
-    {
-        input: customPayload,
-        expected: customPayloadResult,
-        message: 'should correctly process custom payload field',
-        options: { payloadField: 'additional' }
-    },
-    {
-        input: customChildren,
-        expected: commonResult,
-        message: 'should correctly process custom children field',
-        options: { childrenField: 'descedants' }
-    }
-];
 
 describe('flattenTree', function() {
-    tests.forEach(t => {
-        it(t.message, function() {
-            expect(flatten(t.input, t.options)).toEqual(t.expected);
-        });
+    it('should correctly process all fields', function() {
+        const input = {
+            id: 1,
+            title: 'Root node',
+            payload: {},
+            folder: true
+        };
+        const result = flatten(input);
+
+        expect(result).toHaveLength(1);
+        expect(result).toContainEqual(expect.objectContaining({
+            id: 1,
+            title: 'Root node',
+            folder: true,
+            expandable: false,
+            parentId: undefined
+        }));
+    });
+
+    it('should fill payload field with predefined and extra data', function() {
+        const input = {
+            id: 1,
+            title: 'Root node',
+            payload: { predefined: 'predef' },
+            extra: 'extra',
+            folder: true
+        };
+        const result = flatten(input);
+
+        expect(result).toHaveLength(1);
+        expect(result[0]).toHaveProperty('payload', { predefined: 'predef', extra: 'extra' });
+        expect(result[0]).not.toHaveProperty('extra');
+    });
+
+    it('should respect custom payload field name', function() {
+        const input = {
+            id: 1,
+            title: 'Root node',
+            data: { predefined: 'predef' },
+            extra: 'extra',
+            folder: true
+        };
+        const result = flatten(input, { payloadField: 'data' });
+
+        expect(result).toHaveLength(1);
+        expect(result[0]).toHaveProperty('data', { predefined: 'predef', extra: 'extra' });
+        expect(result[0]).not.toHaveProperty('extra');
+        expect(result[0]).not.toHaveProperty('payload');
+    });
+
+    it('should set correct parentId for elements', function() {
+        const commonTree = {
+            id: 1,
+            title: 'Root node',
+            payload: {},
+            folder: true,
+            children: [
+                {
+                    id: 3,
+                    title: 'Second Child',
+                    payload: {},
+                    folder: false
+                }
+            ]
+        };
+        const result = flatten(commonTree);
+
+        expect(result).toHaveLength(2);
+        expect(result).toContainEqual(expect.objectContaining({ parentId: 1, id: 3 }));
+        expect(result).toContainEqual(expect.objectContaining({ parentId: undefined, id: 1 }));
+    });
+
+    it('should respect custom children field name', function() {
+        const commonTree = {
+            id: 1,
+            title: 'Root node',
+            payload: {},
+            folder: true,
+            descedants: [
+                {
+                    id: 3,
+                    title: 'Second Child',
+                    payload: {},
+                    folder: false
+                }
+            ]
+        };
+        const result = flatten(commonTree, { childrenField: 'descedants' });
+
+        expect(result).toHaveLength(2);
+        expect(result).toContainEqual(expect.objectContaining({ parentId: 1, id: 3 }));
+        expect(result).toContainEqual(expect.objectContaining({ parentId: undefined, id: 1 }));
     });
 });
